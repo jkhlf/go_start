d9ad96783cb1de46e57634c737e2e147851b11ba
Joao <joao.khalaf@gmail.com>
2025-03-25 23:16:15 -0300

    demonstrate array and matrix usage, slices

--- main.go
+++ main.go
@@ -1,55 +1,33 @@
 package main
 
-//go build -o main main.go, create a binary file named for compiled machine code,, i can specify the plataform with GOOS=linux GOARCH=amd64 go build -o main main.go
-//the binary file is like a snapshot of the code, if i change the code i need to compile again
-//go run main.go compile and run the code
-
 import (
 	"fmt"
 )
 
+// The Go Programming Language Book
+
 func main() {
 
-	//Variables
-	var name string = "Sung "
-	fmt.Printf("My name is %s\n", name)
+	number := [5]int{10, 20, 30, 40, 50}
+	fmt.Printf("Array %v\n", number)
+	fmt.Print("Array ", number[len(number)-1])
 
-	//Type inference (Go can infer the type of the variable)
-	age := 24
-	fmt.Printf("Age %d\n", age)
+	// numbers := [...] int {10, 20, 30, 40, 50}
 
-	var city string
-	city = "Japan"
-	fmt.Printf("City %s\n", city)
+	matrix := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
 
-	var country, continent string = "Japan", "Asia"
-	fmt.Printf(("Country %s, Continent %s\n"), country, continent)
+	fmt.Printf("Matrix %v\n", matrix)
 
-	var (
-		t bool   = true
-		s int    = 3000
-		p string = "dev"
-	)
-	fmt.Printf("T %t, S %d, P %s\n", t, s, p)
+	// allN := number[:]
+	// first := number[0:3]
 
-	//Zero Values
-	var defaultInt int
-	var defaultFloat float64
-	var defaultBool bool
-	var defaultString string
+	fruits := []string{"apple", "banana", "orange"}
+	fmt.Printf("Fruits %v\n", fruits)
 
-	fmt.Printf("Default Int %d, Default Float %f, Default Bool %t, Default String %s\n", defaultInt, defaultFloat, defaultBool, defaultString)
-
-	//go dont have enums, but we can use constants with iota
-	const (
-		Jan int = iota + 1
-		Feb     //2
-		Mar     //3
-		Apr     //4
-	)
-
-	result, r := add(10, 10)
-	fmt.Printf("Result %d\n", result, r)
+	fruits = append(fruits, "grape")
 
 }
 
